d8cf84657e0dcbd66c20979e833943a144d9fb0e
minh18804 <116489674+minh18804@users.noreply.github.com>
2023-12-21 01:16:12 +0700

    Add files via upload

--- KTLTCCpp.cpp
+++ KTLTCCpp.cpp
@@ -1,114 +1,11 @@
 #include <bits/stdc++.h>
-int binarySearch(int arr[], int l, int r, int x) {
-    if (r >= l) {
-        int mid = l + (r - l) / 2;
-        if (arr[mid] == x)
-            return mid;
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
-        return binarySearch(arr, mid + 1, r, x);
-    }
-    return -1;
-}
-
-
-int jumpSearch(int arr[], int x, int n) {
-    int step = sqrt(n);
-    int prev = 0;
-    while (arr[std::min(step, n) - 1] < x) {
-        prev = step;
-        step += sqrt(n);
-        if (prev >= n)
-            return -1;
-    }
-    while (arr[prev] < x) {
-        prev++;
-        if (prev == std::min(step, n))
-            return -1;
-    }
-    if (arr[prev] == x)
-        return prev;
-
-    return -1;
-}
-
-int linearSearch(int arr[], int target, int n) 
-{
-    for( int i = 0; i < n; i++ ) {
-        if ( arr[i] == target) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int ternarySearch(int l, int r, int key, int ar[]) {
-    if (r >= l) {
-        int mid1 = l + (r - l) / 3;
-        int mid2 = r - (r - l) / 3;
-        if (ar[mid1] == key) {
-            return mid1;
-        }
-        if (ar[mid2] == key) {
-            return mid2;
-        }
-        if (key < ar[mid1]) {
-            return ternarySearch(l, mid1 - 1, key, ar);
-        }
-        else if (key > ar[mid2]) {
-            return ternarySearch(mid2 + 1, r, key, ar);
-        }
-        else {
-            return ternarySearch(mid1 + 1, mid2 - 1, key, ar);
-        }
-    }
-    return -1;
-}
-
-int fibonacciSearch(int arr[], int x, int n){
-    int fibMm2 = 0;
-    int fibMm1 = 1;
-    int fibMm = fibMm2 + fibMm1;
-
-    while (fibMm < n){
-        fibMm2 = fibMm1;
-        fibMm1 = fibMm;
-        fibMm = fibMm2 + fibMm1;
-    }
-
-    int offset = -1;
-    
-    while (fibMm > 1){
-        int i = std::min(offset + fibMm2, n - 1);
-
-        if (arr[i] < x){
-            fibMm = fibMm1;
-            fibMm1 = fibMm2;
-            fibMm2 = fibMm - fibMm1;
-            offset = i;
-        }
-        else if (arr[i] > x){
-            fibMm = fibMm2;
-            fibMm1 = fibMm1 - fibMm2;
-            fibMm2 = fibMm - fibMm1;
-        }
-        else return i;
-    }
-    if (fibMm1 && arr[offset + 1 == x]) return offset + 1;
-}
-
-int exponentialSearch(int arr[], int n, int x) 
-{ 
-
-    if (arr[0] == x) 
-        return 0; 
-  
-    int i = 1; 
-    while (i < n && arr[i] <= x) 
-        i = i*2; 
-  
-    return binarySearch(arr, i/2, std::min(i, n), x); 
-} 
+#include "LinearSearch.h"
+#include "BinarySearch.h"
+#include "JumpSearch.h"
+#include "TernarySearch.h"
+#include "InterpolationSearch.h"
+#include "FibonacciSearch.h"
+#include "ExponentialSearch.h"
 
 int main() {
     using namespace std::chrono;
@@ -144,25 +41,33 @@ int main() {
     exponentialSearch(arr, n-1, x);
     auto end6 = high_resolution_clock::now();
 
+    auto start7 = high_resolution_clock::now();
+    interpolationSearch(arr, 0, n-1, x);
+    auto end7 = high_resolution_clock::now();
+
     auto time_elapsed1 = duration_cast<nanoseconds>(end1 - start1).count();
     auto time_elapsed2 = duration_cast<nanoseconds>(end2 - start2).count();
     auto time_elapsed3 = duration_cast<nanoseconds>(end3 - start3).count();
     auto time_elapsed4 = duration_cast<nanoseconds>(end4 - start4).count();
     auto time_elapsed5 = duration_cast<nanoseconds>(end5 - start5).count();
     auto time_elapsed6 = duration_cast<nanoseconds>(end6 - start6).count();
+    auto time_elapsed7 = duration_cast<nanoseconds>(end7 - start7).count();
 
-    std::cout << "Ket qua cua thuat toan Binary Search: " << binarySearch(arr, 0, n-1, x) << std::endl;
-    std::cout << "Ket qua cua thuat toan Jump Search: " << jumpSearch(arr, x, n) << std::endl;
-    std::cout << "Ket qua cua thuat toan Linear Search: " << linearSearch(arr, x, n) << std::endl;
-    std::cout << "Ket qua cua thuat toan Ternary Search: " << ternarySearch(0, n-1, x, arr) << std::endl;
-    std::cout << "Ket qua cua thuat toan Fibonacci Search: " << fibonacciSearch(arr, x, n-1) << std::endl;
-    std::cout << "Ket qua cua thuat toan Exponential Search: " << exponentialSearch(arr, n-1, x) << std::endl;
+    std::cout << "Ket qua cua thuat toan Binary Search (Tim kiem nhi phan): Tim thay phan tu tai vi tri thu (" << binarySearch(arr, 0, n-1, x) + 1 << ") trong mang" << std::endl;
+    std::cout << "Ket qua cua thuat toan Jump Search (Tim kiem nhay): Tim thay phan tu tai vi tri thu (" << jumpSearch(arr, x, n) + 1 << ") trong mang" << std::endl;
+    std::cout << "Ket qua cua thuat toan Linear Search (Tim kiem tuyen tinh): Tim thay phan tu tai vi tri thu (" << linearSearch(arr, x, n) + 1 << ") trong mang" << std::endl;
+    std::cout << "Ket qua cua thuat toan Ternary Search (Tim kiem tam phan): Tim thay phan tu tai vi tri thu (" << ternarySearch(0, n-1, x, arr) + 1 << ") trong mang" << std::endl;
+    std::cout << "Ket qua cua thuat toan Fibonacci Search (Tim kiem Fibonacci): Tim thay phan tu tai vi tri thu (" << fibonacciSearch(arr, x, n-1) + 1 << ") trong mang" << std::endl;
+    std::cout << "Ket qua cua thuat toan Exponential Search (Tim kiem mu): Tim thay phan tu tai vi tri thu (" << exponentialSearch(arr, n-1, x) + 1 << ") trong mang" << std::endl;
+    std::cout << "Ket qua cua thuat toan Interpolation Search (Tim kiem noi suy): Tim thay phan tu tai vi tri thu (" << interpolationSearch(arr, 0, n-1, x) + 1 << ") trong mang" << std::endl;
+    std::cout << std::endl;
 
-    std::cout << "Thoi gian chay cua thuat toan Binary Search: " << time_elapsed1 << " nanoseconds" << std::endl;
-    std::cout << "Thoi gian chay cua thuat toan Jump Search: " << time_elapsed2 << " nanoseconds" << std::endl;
-    std::cout << "Thoi gian chay cua thuat toan Linear Search: " << time_elapsed3 << " nanoseconds" << std::endl;
-    std::cout << "Thoi gian chay cua thuat toan Ternary Search: " << time_elapsed4 << " nanoseconds" << std::endl;
-    std::cout << "Thoi gian chay cua thuat toan Fibonacci Search: " << time_elapsed5 << "nanoseconds" << std::endl;
-    std::cout << "Thoi gian chay cua thuat toan Exponential Search: " << time_elapsed6 << "nanoseconds" << std::endl;    
+    std::cout << "Thoi gian chay cua thuat toan Binary Search (Tim kiem nhi phan): " << time_elapsed1 << " nanoseconds" << std::endl;
+    std::cout << "Thoi gian chay cua thuat toan Jump Search (Tim kiem nhay): " << time_elapsed2 << " nanoseconds" << std::endl;
+    std::cout << "Thoi gian chay cua thuat toan Linear Search (Tim kiem tuyen tinh): " << time_elapsed3 << " nanoseconds" << std::endl;
+    std::cout << "Thoi gian chay cua thuat toan Ternary Search (Tim kiem tam phan): " << time_elapsed4 << " nanoseconds" << std::endl;
+    std::cout << "Thoi gian chay cua thuat toan Fibonacci Search (Tim kiem Fibonacci): " << time_elapsed5 << " nanoseconds" << std::endl;
+    std::cout << "Thoi gian chay cua thuat toan Exponential Search (Tim kiem mu): " << time_elapsed6 << " nanoseconds" << std::endl;    
+    std::cout << "Thoi gian chay cua thuat toan Interpolation Search (Tim kiem noi suy): " << time_elapsed7 << " nanoseconds" << std::endl;
     return 0;
 }
